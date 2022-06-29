2b83cfcf9a50b69d522b51a6507c39e08531a729
Carllass <Evilangel3211@gmail.com>
2022-06-29 20:33:57 +0500

    added user routers

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+from fastapi import FastAPI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from routers import notes_routes, user_routes
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+app = FastAPI(openapi_url='/openapi.json')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.router.prefix = '/api/v1'
+
+app.include_router(user_routes, prefix='/users')
+app.include_router(notes_routes, prefix='/notes')
