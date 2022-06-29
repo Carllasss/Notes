390967e7dd39226c2164f3bbe44931892b814e62
Carllass <Evilangel3211@gmail.com>
2022-06-29 18:26:07 +0500

    added note crud

--- crud/note_crud.py
+++ crud/note_crud.py
@@ -1,3 +1,49 @@
 from typing import List
 
 from sqlalchemy.orm import Session
+
+from models import notesmodel, usersmodel
+from schemas.notesheme import NoteSchema, NoteUpdateSchema
+
+
+def add_note(
+        db: Session,
+        current_user: usersmodel,
+        note_data: NoteSchema
+):
+    note: notesmodel = notesmodel(
+        text=note_data.text,
+    )
+    note.owner = current_user
+    db.add(note)
+    db.commit()
+    db.refresh(note)
+    return note
+
+
+def update_note(
+        db: Session,
+        new_note: NoteUpdateSchema,
+):
+    note: notesmodel = db.query(notesmodel).filter(notesmodel.id == new_note.id).first()
+    note.text = new_note.text
+    db.commit()
+    db.refresh(note)
+    return note
+
+
+def delete_note(
+        db: Session,
+        note_id: int,
+):
+    note: notesmodel = db.query(notesmodel).filter(notesmodel.id == note_id).first()
+    db.delete(note)
+    db.commit()
+
+
+def get_user_notes(
+        db: Session,
+        current_user: usersmodel,
+) -> List[notesmodel]:
+    notes = db.query(notesmodel).filter(notesmodel.owner == current_user).all()
+    return notes
