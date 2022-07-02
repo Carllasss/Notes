4756c0296032cf34c04ed1f2311fedf12636a1d4
Carllass <Evilangel3211@gmail.com>
2022-07-02 23:23:21 +0500

    more fixes

---
+++ alembic/versions/9ed023ca4149_created_all_tables.py
@@ -0,0 +1,24 @@
+"""created all tables
+
+Revision ID: 9ed023ca4149
+Revises: 
+Create Date: 2022-06-29 20:48:29.608370
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9ed023ca4149'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
